47cfb03577947339a464cb689fcab8b4d36e47fd
nathalia <nathaliatroina@gmail.com>
2022-08-01 10:32:03 -0400

    set up api testing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,21 @@
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import config from '../../key'
 
 function App() {
+
+let [imageData, setImageData] = useState([])
+
+//set up useEffect fot fetching
+console.log(config)
+useEffect(() => {},[])
+
+  
+  
+  // fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true', options)
+  //   .then(response => response.json())
+  //   .then(response => console.log(response))
+  //   .catch(err => console.error(err));
   return (
     <div>
  
