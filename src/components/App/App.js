a68d1aad9336a5fd41268148a884ff22b59d51e7
nathalia <nathaliatroina@gmail.com>
2022-08-01 10:46:00 -0400

    testing api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,17 +8,19 @@ let [imageData, setImageData] = useState([])
 
 //set up useEffect fot fetching
 console.log(config)
-useEffect(() => {},[])
+useEffect(() => {
+  fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=horse&pageNumber=1&pageSize=10&autoCorrect=true', config)
+    .then(response => response.json())
+    .then(response => setImageData(response.value))
+    .catch(err => console.error(err));
+},[])
 
   
-  
-  // fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true', options)
-  //   .then(response => response.json())
-  //   .then(response => console.log(response))
-  //   .catch(err => console.error(err));
+
   return (
     <div>
- 
+      <img src={imageData[0].url} 
+      />
     </div>
   );
 }
