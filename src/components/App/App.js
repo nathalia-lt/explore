d69122805ae466e9786b3f8823d6ce0e2561509c
nathalia <nathaliatroina@gmail.com>
2022-08-01 11:21:02 -0400

    built the core functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,41 @@
 import './App.css';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import config from '../../key'
 
 function App() {
 
-let [imageData, setImageData] = useState([])
+  let [imageData, setImageData] = useState([])
 
-//set up useEffect fot fetching
-console.log(config)
-useEffect(() => {
-  fetch('https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=horse&pageNumber=1&pageSize=10&autoCorrect=true', config)
-    .then(response => response.json())
-    .then(response => setImageData(response.value))
-    .catch(err => console.error(err));
-},[])
+  //set up useEffect fot fetching
+  // console.log(config)
+  // useEffect(() => {
 
-  
+  // },[])
+
+  let [search, setSearch] = useState('')
+
+  function handleSearchChange(event) {
+    setSearch(event.target.value)
+  }
+
+  function handleSubmit() {
+    console.log('hello')
+    let url = 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=' + { search } + '&pageNumber=1&pageSize=10&autoCorrect=true'
+    fetch(url, config)
+      .then(response => response.json())
+      .then(response => setImageData(response.value))
+      .catch(err => console.error(err));
+  }
 
   return (
     <div>
-      <img src={imageData[0].url} 
+      <input type='text'
+        value={search}
+        onChange={handleSearchChange}
       />
+      <button onClick={handleSubmit} > submit </button>
+
+      {imageData[0] ? <img src={imageData[0].url} /> : null}
     </div>
   );
 }
