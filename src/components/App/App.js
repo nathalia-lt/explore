55bae1b3a3b0f1747cf68578a06d6bc5de0f70df
nathalia <nathaliatroina@gmail.com>
2022-08-02 13:31:30 -0400

    update image pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -33,23 +33,45 @@ let [pageNum, setPageNum] = useState(1)
 function handleLeftClick(){
   if(pageNum !== 1){ //eu nao quero que quando eu aperte o botao conte para menos de 1 pagina
   setPageNum(pageNum -1)
+  let url = 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q='+search+'&pageNumber='+(pageNum -1)+'&pageSize=10&autoCorrect=true'
+    console.log(url)
+    fetch(url, config)
+      .then(response => response.json())
+      .then(response => setImageData(response.value))
+      .catch(err => console.error(err));
 }
 }
 
-
+//here when I paste the url I need to change the pageNum on it because of the scope
+//the reason that I had to paste the url here is because I want to change the page number and also update the data
 function handleRightClick(){
   if(pageNum !== 10){ //aqui eu quero que troque no maximo 10 vezes de pagina
   setPageNum(pageNum + 1)
+  let url = 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q='+search+'&pageNumber='+(pageNum+1)+'&pageSize=10&autoCorrect=true'
+    console.log(url)
+    fetch(url, config)
+      .then(response => response.json())
+      .then(response => setImageData(response.value))
+      .catch(err => console.error(err));
 }
 }
 
   //--------------------------------------------------------
 
+//we want to change the results of the pages once we click to go to another page
+
+
+
+
+
+
+
 
   function handleSubmit(e) {
+    setPageNum(1) //aqui toda vez que eu fizer uma nova busca, voltar para a pagina 1 da pesquisa, tambem tenho que ir na url e trocar para 1 inves de pageNum +1
     e.preventDefault() //preventDefault will not refresh the page automaticamente
     console.log('hello')
-    let url = 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q='+search+'&pageNumber=1&pageSize=10&autoCorrect=true'
+    let url = 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q='+search+'&pageNumber='+ 1 +'&pageSize=10&autoCorrect=true'
     console.log(url)
     fetch(url, config)
       .then(response => response.json())
