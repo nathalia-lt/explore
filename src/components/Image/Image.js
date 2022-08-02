b1cb7010bde4be2418f835684db92ae20da03c95
nathalia <nathaliatroina@gmail.com>
2022-08-02 12:27:58 -0400

    image css

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import './Image.css'
 
 
 export default function Image( {image} ){
+
+
+    //aqui eu quero que o subtitle da minha imagem apareca so quando eu hover over it
+    let [titleVisibility, setTitleVisibility] = useState(false)
+
+    function handleMouseOver(){
+        setTitleVisibility(true)
+    }
+
+    let titleClass = titleVisibility ? 'title' : 'title hidden'
+
+    function handleMouseOut(){
+        setTitleVisibility(false)
+    }
+
+
     return(
-        <div className='imageCard' >
+        <div className='imageCard' onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} >
                 <img className='image' src={image.url} />
-                {/* <div className='title'> {image.title} </div> */}
+                <div className={titleClass} > {image.title} </div>
+                {/* here I am applying two classes in once */}
                 </div> 
     )
 }
